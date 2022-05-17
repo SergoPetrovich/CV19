9a91373642af70ddeb97032b36e12e5de5e4b7d5
SP <tk34moodle@tk34.mskobr.ru>
2022-05-17 16:51:27 +0300

    Тест модели представления

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CV19.ViewModels.Base;
 
 namespace CV19.ViewModels
 {
-    internal class MainWindowViewModel
+    internal class MainWindowViewModel:ViewModel
     {
+        private string _Title="Анализ статистики CV19";
+        public string Title
+        {
+            //get { return _Title; }
+            //set { _Title = value; }
+            get => _Title;
+            set=>Set(ref _Title, value);
+        }
     }
 }
